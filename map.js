2a793599cbc1b7168f4973ebc15cdaddab8b43cc
Camilo Andres Vera Ruiz <camilovera9811@gmail.com>
2022-11-28 01:49:51 -0500

    text completed

--- map.js
+++ map.js
@@ -1,54 +1,38 @@
-// Variables para almacenar los elementos del DOM a modificar
-let hobbitonBox = undefined;
-let hobbitonIcon = undefined;
-let rivendellBox = undefined;
-let rivendellIcon = undefined;
-let moriaBox = undefined;
-let moriaIcon = undefined;
-let isengardBox = undefined;
-let isengardIcon = undefined;
-let rohanBox = undefined;
-let rohanIcon = undefined;
-let minasTirithBox = undefined;
-let minasTirithIcon = undefined;
-let minasMorgulBox = undefined;
-let minasMorgulIcon = undefined;
-let mordorBox = undefined;
-let mordorIcon = undefined;
-
 // Objeto con los datos de cada lugar del mapa
 
 data = {
   "hobbiton": {
-    "text": "Hola Hobbiton",
+    "text": "La Comarca (The Shire en inglés) es el nombre que recibe la región de la Tierra Media en la que viven los Hobbits, una raza emparentada con los Hombres que se asentó en esta zona a mediados de la Tercera Edad. Es un extenso país habitado por gente simpática, con buenos caminos, una posada o dos, y aquí y allá un enano o un granjero que trabajaba en paz.",
     "image": "./images/hobbiton.webp"
   },
   "rivendell": {
-    "text": "Hola Rivendell",
+    "text": "Rivendel «Valle Profundo de la Hendedura» fue un refugio élfico en un valle escarpado y recóndito situado en el Ángulo. Llamado Imladris en sindarin y Karningul en oestron." +
+    " Fundado en 1697 de la Segunda Edad por Elrond, que huía de la destrucción de Eregion con los supervivientes de los Gwaith-i-Mírdain; la mayoría de los Elfos de Rivendel eran Eldar, incluyendo a los grandes señores Gildor y Glorfindel.",
     "image": "./images/rivendell.webp"
   },
   "moria": {
-    "text": "Hola Moria",
+    "text": "Moria «Pozo Oscuro» es el nombre que reciben las más grandes minas construidas por los Enanos en la Tierra Media, situadas más al sur en las Montañas Nubladas. Conocida también como Khazad-dûm (mansión de los enanos), la Mina del Enano, fue durante mucho tiempo el símbolo del poderío del pueblo Enano, siendo un gran centro de comercio y minería a pesar de encontrarse algo alejada de otras minas enanas. Fue el más antiguo y famoso de los reinos de los enanos. Sin embargo, la prosperidad terminó cuando los Enanos excavaron demasiado hondo en busca de mithril, y despertaron a un antiguo Balrog que se encontraba en letargo en lo más profundo de la tierra. Los Enanos abandonaron entonces Khazad-dûm, y las minas se convirtieron en un lugar de oscuridad y miedo.",
     "image": "./images/moria.jpg"
   },
   "isengard": {
-    "text": "Hola Isengard",
+    "text": "Isengard, también conocida como Angrenost «Fortaleza de Hierro», fue una fortaleza situada en el extremo sur de las Montañas Nubladas." +
+    " Fue una fortaleza de Gondor durante la Tercera Edad, en ella se encontraba una de las Palantiri. Allí habitó el mago Saruman durante los últimos años de la Tercera Edad, y fue en Isengard donde creó un gran ejército para intentar apoderarse del Anillo Único y dominar la Tierra Media.",
     "image": "./images/isengard.webp"
   },
   "rohan": {
-    "text": "Hola Rohan",
+    "text": "Rohan es un reino fundado por los Hombres del Norte, los Eorlingas, que ocupa las grandes llanuras cubiertas de pastos situadas al norte de las Montañas Blancas y al este de las Montañas Nubladas. Sus habitantes descienden de los hombres de Éothéod, y se caracterizan por ser amantes de los caballos y por ser los principales aliados del reino de Gondor. Tiene especial concurrencia en los acontecimientos de El Señor de los Anillos, pues los Rohirrim constituyen uno de los principales bandos que luchan contra Sauron.",
     "image": "./images/rohan.webp"
   },
   "minas-tirith": {
-    "text": "Hola Minas Tirith",
+    "text": "Aqui se encuetra la ciudad de Minas Tirth, para mas información consulte las demás pestañas de la pagina.",
     "image": "./images/minas-tirith.jpg"
   },
   "minas-morgul": {
-    "text": "Hola Minas Morgul",
+    "text": "En un principio fue llamada Minas Ithil, ciudad fundada por Isildur en el 3320 de la Segunda Edad, sobre una estribación de las Montañas de la Sombra, la ciudad fue gobernada por Isildur y se convirtió en capital de la región. Después sería tomada por Sauron, pero volvió a manos de los hombres tras la Batalla de la Última Alianza. Sin embargo, una peste dejó despoblada la ciudad en el año 1636 de la Tercera Edad.",
     "image": "./images/minas-morgul.jpg"
   },
   "mordor": {
-    "text": "Hola Mordor",
+    "text": "Mordor es un país situado al sureste de la Tierra Media, que tuvo gran importancia durante la Guerra del Anillo por ser el lugar donde Sauron, el Señor Oscuro, decidió edificar su fortaleza de Barad-dûr para intentar atacar y dominar a todos los pueblos de la Tierra Media. Se trata de una región desolada, rodeada de montañas con un interior desértico sin vegetación.",
     "image": "./images/mordor.webp"
   }
 }
@@ -84,7 +68,6 @@ window.onload = function() {
   let mordorIcon = svgDoc.getElementById("mordor-icon");
 
   // Aplicar estilos a los elementos del svg (debido a que no se pueden aplicar desde el css)
-
   // Pointer para mostrar el cursor al pasar por encima de los iconos
   hobbitonBox.style.cursor = "pointer";
   hobbitonIcon.style.cursor = "pointer";
@@ -103,34 +86,26 @@ window.onload = function() {
   mordorBox.style.cursor = "pointer";
   mordorIcon.style.cursor = "pointer";
 
-  // Hovers para los iconos
-  // hobbitonBox.addEventListener("mouseover", function() {
-  //   hobbitonIcon.style.width = "100px";
-  // });
-  // hobbitonBox.addEventListener("mouseout", function() {
-  //   hobbitonIcon.style.width = "30px";
-  // });
-
-
+  // Variables con los elementos del DOM
   let descriptionBox = document.getElementById("description-box");
+  let descriptionText = descriptionBox.childNodes[2];
   let descriptionImage = document.getElementById("description-image");
 
 
   // Función genérica para mostrar la descripción e imagen de cada lugar, recibe como parámetro el ID del lugar y retorna
   // la función que se ejecutará al hacer click en el lugar, esto es necesario ya que el evento click recibe una función como parámetro
-
   function clicEvent(locationID){
     return function(){
       if(boxState == locationID) {
-        descriptionBox.style.display = "none";              // Ocultar la caja de descripción
-        window.location.href = "#interactive-map";          // Mover el scroll hasta el mapa interactivo
-        boxState = "hidden";                                // Modificar la variable que contiene el estado de la caja de descripción
+        descriptionBox.style.display = "none";                // Ocultar la caja de descripción
+        window.location.href = "#interactive-map";            // Mover el scroll hasta el mapa interactivo
+        boxState = "hidden";                                  // Modificar la variable que contiene el estado de la caja de descripción
       }else{
-        // descriptionBox.innerText = data[locationID]["text"];
-        descriptionImage.src = data[locationID]["image"];   // Cambiar la imagen de la caja de descripción
-        descriptionBox.style.display = "block";             // Mostrar la caja de descripción
-        window.location.href = "#description-box";          // Mover el scroll hasta la caja de descripción
-        boxState = locationID;                              // Modificar la variable que contiene el estado de la caja de descripción
+        descriptionText.nodeValue = data[locationID]["text"]; // Cambiar el texto de la caja de descripción
+        descriptionImage.src = data[locationID]["image"];     // Cambiar la imagen de la caja de descripción
+        descriptionBox.style.display = "block";               // Mostrar la caja de descripción
+        window.location.href = "#description-box";            // Mover el scroll hasta la caja de descripción
+        boxState = locationID;                                // Modificar la variable que contiene el estado de la caja de descripción
       }
     }
   }
@@ -155,12 +130,4 @@ window.onload = function() {
   mordorBox.addEventListener("click", clicEvent("mordor"));
   mordorIcon.addEventListener("click", clicEvent("mordor"));
 
-
-
-
-
-
 }
-
-console.log("Funciona?");
-
